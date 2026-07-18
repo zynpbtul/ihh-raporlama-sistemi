0368e97a8f870a14f15fb47bdaef85c0e1ed90a1
Rabia Okutan <rabia.okutan@ogr.sakarya.edu.tr>
2026-07-18 13:05:04 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-{
-  "name": "Genç İHH",
-  "description": "Genç İHH üniversite teşkilatlarının haftalık faaliyetlerini raporlayan sistem.",
-  "requestFramePermissions": [],
-  "majorCapabilities": ["MAJOR_CAPABILITY_SERVER_SIDE_GEMINI_API"]
-}
